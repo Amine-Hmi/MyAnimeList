a48c1b4d44baa3f3268bf3ef0e48365c2fb93358
Amine-Hmi <hammemi.amine@gmail.com>
2020-12-29 23:31:56 +0100

    Updates

---
+++ precache-manifest.d68e5ae41fe192363e7f65ea2da0720c.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "13602e6f21451e4be5d4099dea0d3de2",
+    "url": "/Amine-Hmi/MyAnimeList/index.html"
+  },
+  {
+    "revision": "4dfe99a610d34d5b0149",
+    "url": "/Amine-Hmi/MyAnimeList/static/css/main.2ac089f9.chunk.css"
+  },
+  {
+    "revision": "3c04213fb9df68d7bfaa",
+    "url": "/Amine-Hmi/MyAnimeList/static/js/2.b8144d84.chunk.js"
+  },
+  {
+    "revision": "2d8328393d82966fa42f594b317ea365",
+    "url": "/Amine-Hmi/MyAnimeList/static/js/2.b8144d84.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4dfe99a610d34d5b0149",
+    "url": "/Amine-Hmi/MyAnimeList/static/js/main.546af488.chunk.js"
+  },
+  {
+    "revision": "8130e5f5df6a7d7969e6",
+    "url": "/Amine-Hmi/MyAnimeList/static/js/runtime-main.23aba189.js"
+  },
+  {
+    "revision": "fffa31e7b5870ee226f37bf7b0a8ffb8",
+    "url": "/Amine-Hmi/MyAnimeList/static/media/bg.fffa31e7.jpg"
+  },
+  {
+    "revision": "22e5f56b17aeced6dc7f69c8d422a1ab",
+    "url": "/Amine-Hmi/MyAnimeList/static/media/default_cover.22e5f56b.png"
+  },
+  {
+    "revision": "2fd48a92efd8da4d33050d3b03aabd72",
+    "url": "/Amine-Hmi/MyAnimeList/static/media/logo.2fd48a92.png"
+  },
+  {
+    "revision": "e599c8756bd019eac230c44154e7733d",
+    "url": "/Amine-Hmi/MyAnimeList/static/media/missing.e599c875.jpg"
+  },
+  {
+    "revision": "cf8069524d49ad0fc442dd9e81d30ece",
+    "url": "/Amine-Hmi/MyAnimeList/static/media/spinner3.cf806952.gif"
+  }
+]);
--- src/components/axios.js
+++
@@ -1,8 +0,0 @@
-import axios from "axios"
-
-const instance = axios.create({
-
-    baseURL: "https:kitsu.io/api/edge/"
-});
-
-export default instance;
